bd0f8a7f70fd456313dab428797d55b01d2ab6b2
Daniel Hopler <hopler.daniel@gmail.com>
2018-12-03 11:19:14 -0500

    Need to fix random_date for seeding data

--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Wiki, type: :model do
-  let(:my_user) { User.create!(username: "Blocipedia User", email: "user@blocipedia.com", password: "password" ) }
-  let(:my_wiki) { Wiki.create!(title: "New Wiki Title", body: "New Wiki Body", user: my_user) }
+  let(:user) { User.create!(username: "Blocipedia User", email: "user@blocipedia.com", password: "password" ) }
+  let(:wiki) { Wiki.create!(title: "New Wiki Title", body: "New Wiki Body", user: user) }
 
 
 
   describe "attributes" do
     it "has title and body attributes" do
-      expect(my_wiki).to have_attributes( title: "New Wiki Title", body: "New Wiki Body" )
+      expect(wiki).to have_attributes( title: "New Wiki Title", body: "New Wiki Body", user: user )
     end
   end
 
