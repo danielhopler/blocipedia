7bb56f21e3f3c81c51601d6d2fd2b35098b02de4
Daniel Hopler <hopler.daniel@gmail.com>
2018-12-11 12:04:39 -0500

    CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,28 +4,3 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-require 'random_data'
-
-5.times do
-   User.create!(
- # #3
-   username: RandomData.random_name,
-   email:    RandomData.random_email,
-   password: "password"
-   )
- end
- users = User.all
-
- 20.times do
-	Wiki.create!(
-		user:   users.sample,
-		title:  RandomData.sentence,
-		body:   RandomData.paragraph
-	)
-end
-posts = Post.all
-
- puts "Seed finished"
- puts "#{User.count} users created"
- puts "#{Wiki.count} topics created"
