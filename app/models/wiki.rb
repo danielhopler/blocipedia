0e30a77754fe59727abb8b69e1176d7a30ac8881
Daniel Hopler <hopler.daniel@gmail.com>
2018-12-19 12:38:04 -0500

    Adding partial for wiki views and validations. Need shoulda gem

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,5 +1,9 @@
 class Wiki < ApplicationRecord
   belongs_to :user
 
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 5 }, presence: true
+
+
 
 end
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe Wiki, type: :model do
   let(:wiki) { Wiki.create!(title: "New Wiki Title", body: "New Wiki Body", user: user) }
 
 
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(5) }
+
+
 
   describe "attributes" do
     it "has title and body attributes" do
