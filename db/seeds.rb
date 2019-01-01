39ba5ae699964003e8b5aa328b70f58b811255d4
Daniel Hopler <hopler.daniel@gmail.com>
2019-01-01 11:33:08 -0500

    Add Faker to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,58 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+require 'random_data'
+require 'faker'
+
+5.times do
+  User.create!(
+    username: Faker::HarryPotter.character,
+    email: Faker::Internet.email,
+    password: 'password',
+    confirmed_at: Time.now
+  )
+end
+
+users = User.all
+
+20.times do
+  Wiki.create!(
+    user: users.sample,
+    title: Faker::HarryPotter.book,
+    body: Faker::HarryPotter.quote
+  )
+end
+
+wikis = Wiki.all
+
+admin = User.create!(
+  username: 'Admin',
+  email: 'admin@example.com',
+  password: 'password',
+  confirmed_at: Time.now,
+  role: 'admin'
+)
+
+premium = User.create!(
+  username: 'Premium',
+  email: 'premium@example.com',
+  password: 'password',
+  confirmed_at: Time.now,
+  role: 'premium'
+)
+
+standard = User.create!(
+  username: 'Standard',
+  email: 'stamdard@example.com',
+  password: 'password',
+  confirmed_at: Time.now,
+)
+
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
+
+puts "#{User.standard.count} standard users created"
+puts "#{User.admin.count} admin created"
+puts "#{User.premium.count} premium created"
